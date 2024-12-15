bca90f78e2d6a3dd34a5c820b9375325553f840f
Oseni03 <oseniayomide57@gmail.com>
2024-12-15 02:17:16 +0100

    refactor: modify default template

--- components/templates/main/_components/resume-card.jsx
+++ components/templates/main/_components/resume-card.jsx
@@ -34,7 +34,7 @@ export const ResumeCard = ({
 			className="block cursor-pointer"
 			onClick={handleClick}
 		>
-			<Card className="flex items-center border-0">
+			<Card className="flex border border-hidden">
 				<div className="flex-none">
 					<Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
 						<AvatarImage
@@ -46,7 +46,7 @@ export const ResumeCard = ({
 					</Avatar>
 				</div>
 				<div className="flex-grow ml-4 items-center flex-col group">
-					<CardHeader>
+					<CardHeader className="p-0">
 						<div className="flex items-center justify-between gap-x-2 text-base">
 							<h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
 								{title}
--- components/templates/main/default-template.jsx
+++ components/templates/main/default-template.jsx
@@ -63,209 +63,225 @@ export default function DefaultTemplate({
 					{HTMLReactParser(basics.about)}
 				</BlurFade>
 			</section>
-			<section id="work">
-				<div className="flex min-h-0 flex-col gap-y-3">
-					<BlurFade delay={BLUR_FADE_DELAY * 5}>
-						<h2 className="text-xl font-bold">Work Experience</h2>
-					</BlurFade>
-					{experiences.items.map((work, id) => (
-						<BlurFade
-							key={work.company}
-							delay={BLUR_FADE_DELAY * 6 + id * 0.05}
-						>
-							<ResumeCard
+			{experiences.items.length > 0 && (
+				<section id="work">
+					<div className="flex min-h-0 flex-col gap-y-3">
+						<BlurFade delay={BLUR_FADE_DELAY * 5}>
+							<h2 className="text-xl font-bold">
+								Work Experience
+							</h2>
+						</BlurFade>
+						{experiences.items.map((work, id) => (
+							<BlurFade
 								key={work.company}
-								// logoUrl={work.logoUrl}
-								altText={work.company}
-								title={work.company}
-								subtitle={work.position}
-								href={work.url}
-								badges={work.badges}
-								period={work.date}
-								description={HTMLReactParser(work.summary)}
-							/>
-						</BlurFade>
-					))}
-				</div>
-			</section>
-			<section id="education">
-				<div className="flex min-h-0 flex-col gap-y-3">
-					<BlurFade delay={BLUR_FADE_DELAY * 7}>
-						<h2 className="text-xl font-bold">Education</h2>
-					</BlurFade>
-					{educations.items.map((edu, id) => (
-						<BlurFade
-							key={edu.institution}
-							delay={BLUR_FADE_DELAY * 8 + id * 0.05}
-						>
-							<ResumeCard
-								key={edu.institution}
-								href={edu.url}
-								// logoUrl={education.logoUrl}
-								altText={edu.institution}
-								title={edu.institution}
-								subtitle={edu.studyType}
-								// period={`${education.start} - ${education.end}`}
-								period={edu.date}
-							/>
-						</BlurFade>
-					))}
-				</div>
-			</section>
-			<section id="skills">
-				<div className="flex min-h-0 flex-col gap-y-3">
-					<BlurFade delay={BLUR_FADE_DELAY * 9}>
-						<h2 className="text-xl font-bold">Skills</h2>
-					</BlurFade>
-					<div className="flex flex-wrap gap-1">
-						{skills.items.map((skill, id) => (
-							<BlurFade
-								key={skill.id}
-								delay={BLUR_FADE_DELAY * 10 + id * 0.05}
+								delay={BLUR_FADE_DELAY * 6 + id * 0.05}
 							>
-								<Badge key={skill.id}>{skill.name}</Badge>
-							</BlurFade>
-						))}
-					</div>
-				</div>
-			</section>
-			<section id="projects">
-				<div className="space-y-12 w-full py-12">
-					<BlurFade delay={BLUR_FADE_DELAY * 11}>
-						<div className="flex flex-col items-center justify-center space-y-4 text-center">
-							<div className="space-y-2">
-								<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-									My Projects
-								</div>
-								<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-									Check out my latest work
-								</h2>
-								<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-									I&apos;ve worked on a variety of projects,
-									from simple websites to complex web
-									applications. Here are a few of my
-									favorites.
-								</p>
-							</div>
-						</div>
-					</BlurFade>
-					<div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
-						{projects.items.map((project, id) => (
-							<BlurFade
-								key={project.id}
-								delay={BLUR_FADE_DELAY * 12 + id * 0.05}
-							>
-								<ProjectCard
-									href={project.url}
-									key={project.name}
-									title={project.name}
-									description={HTMLReactParser(
-										project.description
-									)}
-									dates={project.date}
-									tags={project.technologies}
-									image={project.image}
-									links={project.links}
+								<ResumeCard
+									key={work.company}
+									logoUrl={work.picture}
+									altText={work.company}
+									title={work.company}
+									subtitle={work.position}
+									href={work.url}
+									badges={work.badges}
+									period={work.date}
+									description={HTMLReactParser(work.summary)}
 								/>
 							</BlurFade>
 						))}
 					</div>
-				</div>
-			</section>
-			<section id="certifications">
-				<div className="space-y-12 w-full py-12">
-					<BlurFade delay={BLUR_FADE_DELAY * 13}>
-						<div className="flex flex-col items-center justify-center space-y-4 text-center">
-							<div className="space-y-2">
-								<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-									Certifications
-								</div>
-								<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-									I value learning
-								</h2>
-								<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-									Explore the professional certifications I
-									have earned throughout my career, showcasing
-									my commitment to continuous learning and
-									expertise in various fields. These
-									certifications reflect my proficiency in key
-									technologies, methodologies, and industry
-									best practices, helping me stay at the
-									forefront of innovation and ensuring that I
-									deliver high-quality solutions.
-								</p>
-							</div>
-						</div>
-					</BlurFade>
-					<BlurFade delay={BLUR_FADE_DELAY * 14}>
-						<ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-							{certifications.items.map((project, id) => (
+				</section>
+			)}
+			{educations.items.length > 0 && (
+				<section id="education">
+					<div className="flex min-h-0 flex-col gap-y-3">
+						<BlurFade delay={BLUR_FADE_DELAY * 7}>
+							<h2 className="text-xl font-bold">Education</h2>
+						</BlurFade>
+						{educations.items.map((edu, id) => (
+							<BlurFade
+								key={edu.institution}
+								delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+							>
+								<ResumeCard
+									key={edu.institution}
+									href={edu.url}
+									// logoUrl={education.logoUrl}
+									altText={edu.institution}
+									title={edu.institution}
+									subtitle={edu.studyType}
+									// period={`${education.start} - ${education.end}`}
+									period={edu.date}
+								/>
+							</BlurFade>
+						))}
+					</div>
+				</section>
+			)}
+			{skills.items.length > 0 && (
+				<section id="skills">
+					<div className="flex min-h-0 flex-col gap-y-3">
+						<BlurFade delay={BLUR_FADE_DELAY * 9}>
+							<h2 className="text-xl font-bold">Skills</h2>
+						</BlurFade>
+						<div className="flex flex-wrap gap-1">
+							{skills.items.map((skill, id) => (
 								<BlurFade
-									key={project.id}
-									delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+									key={skill.id}
+									delay={BLUR_FADE_DELAY * 10 + id * 0.05}
 								>
-									<CertificationCard
-										name={project.name}
-										issuer={project.issuer}
-										summary={HTMLReactParser(
-											project.summary
-										)}
-										location={project.location}
-										date={project.date}
-										url={project.url}
-									/>
+									<Badge key={skill.id}>{skill.name}</Badge>
 								</BlurFade>
 							))}
-						</ul>
-					</BlurFade>
-				</div>
-			</section>
-			<section id="hackathons">
-				<div className="space-y-12 w-full py-12">
-					<BlurFade delay={BLUR_FADE_DELAY * 13}>
-						<div className="flex flex-col items-center justify-center space-y-4 text-center">
-							<div className="space-y-2">
-								<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-									Hackathons
-								</div>
-								<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-									I like building things
-								</h2>
-								<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-									During my time in university, I attended{" "}
-									{hackathons.items.length}+ hackathons.
-									People from around the country would come
-									together and build incredible things in 2-3
-									days. It was eye-opening to see the endless
-									possibilities brought to life by a group of
-									motivated and passionate individuals.
-								</p>
-							</div>
 						</div>
-					</BlurFade>
-					<BlurFade delay={BLUR_FADE_DELAY * 14}>
-						<ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-							{hackathons.items.map((project, id) => (
+					</div>
+				</section>
+			)}
+			{projects.items.length > 0 && (
+				<section id="projects">
+					<div className="space-y-12 w-full py-12">
+						<BlurFade delay={BLUR_FADE_DELAY * 11}>
+							<div className="flex flex-col items-center justify-center space-y-4 text-center">
+								<div className="space-y-2">
+									<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+										My Projects
+									</div>
+									<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+										Check out my latest work
+									</h2>
+									<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+										I&apos;ve worked on a variety of
+										projects, from simple websites to
+										complex web applications. Here are a few
+										of my favorites.
+									</p>
+								</div>
+							</div>
+						</BlurFade>
+						<div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+							{projects.items.map((project, id) => (
 								<BlurFade
 									key={project.id}
-									delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+									delay={BLUR_FADE_DELAY * 12 + id * 0.05}
 								>
-									<HackathonCard
+									<ProjectCard
+										href={project.url}
+										key={project.name}
 										title={project.name}
 										description={HTMLReactParser(
 											project.description
 										)}
-										location={project.location}
 										dates={project.date}
-										image={project.logo || null}
+										tags={project.technologies}
+										image={project.image}
 										links={project.links}
 									/>
 								</BlurFade>
 							))}
-						</ul>
-					</BlurFade>
-				</div>
-			</section>
+						</div>
+					</div>
+				</section>
+			)}
+			{certifications.items.length > 0 && (
+				<section id="certifications">
+					<div className="space-y-12 w-full py-12">
+						<BlurFade delay={BLUR_FADE_DELAY * 13}>
+							<div className="flex flex-col items-center justify-center space-y-4 text-center">
+								<div className="space-y-2">
+									<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+										Certifications
+									</div>
+									<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+										I value learning
+									</h2>
+									<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+										Explore the professional certifications
+										I have earned throughout my career,
+										showcasing my commitment to continuous
+										learning and expertise in various
+										fields. These certifications reflect my
+										proficiency in key technologies,
+										methodologies, and industry best
+										practices, helping me stay at the
+										forefront of innovation and ensuring
+										that I deliver high-quality solutions.
+									</p>
+								</div>
+							</div>
+						</BlurFade>
+						<BlurFade delay={BLUR_FADE_DELAY * 14}>
+							<ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+								{certifications.items.map((project, id) => (
+									<BlurFade
+										key={project.id}
+										delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+									>
+										<CertificationCard
+											name={project.name}
+											issuer={project.issuer}
+											summary={HTMLReactParser(
+												project.summary
+											)}
+											location={project.location}
+											date={project.date}
+											url={project.url}
+										/>
+									</BlurFade>
+								))}
+							</ul>
+						</BlurFade>
+					</div>
+				</section>
+			)}
+			{hackathons.items.length > 0 && (
+				<section id="hackathons">
+					<div className="space-y-12 w-full py-12">
+						<BlurFade delay={BLUR_FADE_DELAY * 13}>
+							<div className="flex flex-col items-center justify-center space-y-4 text-center">
+								<div className="space-y-2">
+									<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+										Hackathons
+									</div>
+									<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+										I like building things
+									</h2>
+									<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+										During my time in university, I attended{" "}
+										{hackathons.items.length}+ hackathons.
+										People from around the country would
+										come together and build incredible
+										things in 2-3 days. It was eye-opening
+										to see the endless possibilities brought
+										to life by a group of motivated and
+										passionate individuals.
+									</p>
+								</div>
+							</div>
+						</BlurFade>
+						<BlurFade delay={BLUR_FADE_DELAY * 14}>
+							<ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+								{hackathons.items.map((project, id) => (
+									<BlurFade
+										key={project.id}
+										delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+									>
+										<HackathonCard
+											title={project.name}
+											description={HTMLReactParser(
+												project.description
+											)}
+											location={project.location}
+											dates={project.date}
+											image={project.logo || null}
+											links={project.links}
+										/>
+									</BlurFade>
+								))}
+							</ul>
+						</BlurFade>
+					</div>
+				</section>
+			)}
 			<section id="contact">
 				<div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
 					<BlurFade delay={BLUR_FADE_DELAY * 16}>
